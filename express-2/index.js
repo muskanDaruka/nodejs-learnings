3df4677a5915f1919d0f2e631b481aff3a6e60b3
muskanDaruka <darukamuskan@gmail.com>
2026-07-18 23:20:18 +0530

    fix: added space

--- express-2/index.js
+++ express-2/index.js
@@ -8,13 +8,13 @@ server.use((req, res, next)=>{                  //middleware
     next()
 })
 
-server.get("/", (req,res, next) => {          //route handler    //if we use next() here than only it will run the after 
+server.get("/", (req, res, next) => {          //route handler    //if we use next() here than only it will run the after 
     console.log(2)
     res.send("Server is running")
     next()
 })
 
-server.use((req, res, next)=>{            //after route handler runs the code after that  will never run, even its a middleware
+server.use((req, res, next) => {            //after route handler runs the code after that  will never run, even its a middleware
     console.log(3)
     next()
 })
